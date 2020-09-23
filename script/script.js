47f5043d1f472e3607a77188ea3e12cf55a81fa7
jeckenot <70687182+jeckenot@users.noreply.github.com>
2020-09-23 15:07:53 +0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -26,35 +26,56 @@ $(function () {
       "transition-timing-function": "ease-in",
       "transition-delay": "0.15s"
 
-    })
+    });
     $(".anim_poster .blue_line ").css({
       "left": "2000px",
       "transition": "3.5s",
       "transition-timing-function": "ease-in",
-      "transition-delay": "0.17s"
+      "transition-delay": "0.178s"
 
-    })
-    $(".anim_poster #top_poster1, .yellow_line").css({
+    });
+    $(".anim_poster #top_poster1").css({
       "left": "2000px",
       "transition": "3.5s",
       "transition-timing-function": "ease-in",
       "transition-delay": "0.22s"
 
-    })
-    $(".anim_poster #top_poster_mirrored, .blue_line_mirrored").css({
+    });
+    $(".anim_poster .yellow_line").css({
+      "left": "2000px",
+      "transition": "3.5s",
+      "transition-timing-function": "ease-in",
+      "transition-delay": "0.248s"
+
+    });
+    $(".anim_poster #top_poster_mirrored").css({
       "left": "-2000px",
       "transition": "3s",
       "transition-timing-function": "ease-in",
       "transition-delay": "3.8s"
 
-    })
-    $(".anim_poster #top_poster1_mirrored, .yellow_line_mirrored").css({
+    });
+    $(".anim_poster .blue_line_mirrored").css({
+      "left": "-2000px",
+      "transition": "3s",
+      "transition-timing-function": "ease-in",
+      "transition-delay": "3.928s"
+
+    });
+    $(".anim_poster #top_poster1_mirrored").css({
       "left": "-2000px",
       "transition": "3s",
       "transition-timing-function": "ease-in",
       "transition-delay": "3.85s"
 
-    })
+    });
+    $(".anim_poster .yellow_line_mirrored").css({
+      "left": "-2000px",
+      "transition": "3s",
+      "transition-timing-function": "ease-in",
+      "transition-delay": "3.978s"
+
+    });
 
 
 
