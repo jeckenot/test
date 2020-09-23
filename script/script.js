089d858f050f50246a812cb62e62349af776e399
jeckenot <70687182+jeckenot@users.noreply.github.com>
2020-09-23 15:24:54 +0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -31,7 +31,7 @@ $(function () {
       "left": "2000px",
       "transition": "3.5s",
       "transition-timing-function": "ease-in",
-      "transition-delay": "0.15s"
+      "transition-delay": "0.178s"
 
     });
     $(".anim_poster #top_poster1").css({
@@ -45,7 +45,7 @@ $(function () {
       "left": "2000px",
       "transition": "3.5s",
       "transition-timing-function": "ease-in",
-      "transition-delay": "0.238s"
+      "transition-delay": "0.248s"
 
     });
     $(".anim_poster #top_poster_mirrored").css({
@@ -59,7 +59,7 @@ $(function () {
       "left": "-2000px",
       "transition": "3s",
       "transition-timing-function": "ease-in",
-      "transition-delay": "3.918s"
+      "transition-delay": "3.928s"
 
     });
     $(".anim_poster #top_poster1_mirrored").css({
@@ -73,7 +73,7 @@ $(function () {
       "left": "-2000px",
       "transition": "3s",
       "transition-timing-function": "ease-in",
-      "transition-delay": "3.968s"
+      "transition-delay": "3.978s"
 
     });
 
